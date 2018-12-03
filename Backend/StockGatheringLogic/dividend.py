fd2730a4f0d96e8081309355172f9c8826ca9854
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2018-12-03 08:46:54 -0200

    Lógica para montagem da lista de empresas pagadoras de dividendo

---
+++ Backend/StockGatheringLogic/dividend.py
@@ -0,0 +1,35 @@
+import urllib, json, sys, pprint
+from models.dividendModel import dividendModel
+
+### GLOBAL CONSTANTS ###
+SERVICE_ENDPOINT = "https://www.bussoladoinvestidor.com.br/nb/api/v1/stocks"
+STOCK_TYPE_TO_FILTER = "ON" # Leave it empty for all types
+
+# FIELDS #
+FIELD_RESULTS = "results"
+FIELD_TYPE = "type"
+
+# METHODS #
+def filterStocks(stocks, type):
+        return [s for s in stocks[FIELD_RESULTS] if s[FIELD_TYPE] == type]
+
+def getDividendModel(stocks):
+        returnObj = []
+        for stock in stocks:
+                dividendModelAux = dividendModel()
+                dividendModelAux.Code = stock["code"]
+                dividendModelAux.Company = stock["name"]
+                dividendModelAux.Type = stock[FIELD_TYPE]
+                dividendModelAux.StockPrice = float(str(stock["price"]).replace(',', '.')) 
+                dividendModelAux.Sector = stock["sector"]
+                dividendModelAux.Equity = float(stock["equity"]) # float(str(stock["equity"]).replace(',', '.'))       
+                returnObj.append(dividendModelAux)
+        return returnObj
+
+def getStocksJson():
+        return open("C:\git\SoSI\Backend\StockGatheringLogic\mock\dividendMock.json", 'r') # Mock for tests
+        # return urllib.request.urlopen(SERVICE_ENDPOINT) # Production
+
+stocks = json.load(getStocksJson())
+filteredStocks = filterStocks(stocks, STOCK_TYPE_TO_FILTER)
+divdendObj = getDividendModel(filteredStocks)
---
+++ Backend/StockGatheringLogic/models/dividendModel.py
@@ -0,0 +1,35 @@
+class dividendModel:
+    Code: ""
+    Company: ""
+    Sector: ""
+    SecondSector: ""
+    StockPrice: 0.00
+    Type: ""
+    StockAvailableAmount: 0
+    Valuation: 0.00
+    Equity: 0.00
+    Avarage21Negociation: 0
+    DividendPeriod: ""
+    DividendLastPrice: 0.00
+    DividendTotalValueShared: 0.00
+    NetProfit: 0.00
+    DividendYeld: 0.00
+    AvgPayoutTwelveMonths: 0.00
+    AvgPayoutFiveYears: 0.00
+    MajorShareholder: ""
+
+    def setAvaragePayout(self, payoutArray, netProfitArray):
+        return (sum(payoutArray, 0)/ (sum(payoutArray, 0) if netProfitArray != 0 else 1))
+    
+    def setAvaragePayoutTwelveMonth(self, payoutArray, netProfitArray):
+        self.AvgPayoutTwelveMonths = self.setAvaragePayout(payoutArray, netProfitArray)
+
+    def setAvaragePayoutFiveYeats(self, payoutArray, netProfitArray):
+        self.AvgPayoutFiveYears = self.setAvaragePayout(payoutArray, netProfitArray)
+
+    def setValuation(self):
+        self.Valuation = (self.StockPrice * self.StockAvailableAmount)
+    
+    def setDividendYield(self):
+        self.DividendYeld = self.DividendLastPrice/(self.StockPrice if (self.StockPrice != 0) else 1)
+    
