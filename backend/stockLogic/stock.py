8faeadd6ce933048041b88e4366f439a556eb812
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2018-12-13 11:13:40 -0200

    Ajustes na lógica de obtenção das informações básicas

--- backend/stockLogic/stock.py
+++ backend/stockLogic/stock.py
@@ -1,7 +1,12 @@
 import sys
 import requests
 import urllib
+
+# ADDING ITEMS TO SYS.PATH #
+sys.path.append("\\git\\SoSI\\backend")
+
 from bs4 import BeautifulSoup
+from helpers.parser import Parser
 
 #############################
 ##                         ##   
@@ -19,12 +24,13 @@ class Stock(object):
         self.AvailableStockCode = self.GetAvailableStocks()
         print("BOVESPA available stocks successfully acquired!")
         self.StocksBasicInfo = self.GetStocksBasicInfo()
-        print("BOVESPA stocks offered amount successfully acquired!")
+        print("BOVESPA offered amount by stock successfully acquired!")
 
     def GetAvailableStocks(self):
         __availableStocksSingleton = None
 
-        htmlPage = urllib.request.urlopen(self.MAIN_URL % "detalhes.php")
+        urlAux = self.MAIN_URL % "detalhes.php/"
+        htmlPage = urllib.request.urlopen(urlAux)
         stocksPage = BeautifulSoup(htmlPage.read(), features="html.parser")
 
         if stocksPage is None:
@@ -66,6 +72,9 @@ class Stock(object):
         return __availableStocksSingleton
 
     def GetStocksBasicInfo(self):
+        returnList = []
+        stockBasicInfoList = {}
+
         if (len(self.AvailableStockCode) == 0):
             self.AvailableStockCode = self.GetAvailableStocks()
 
@@ -73,7 +82,31 @@ class Stock(object):
             htmlPage = urllib.request.urlopen(self.MAIN_URL % stock[0])
             stocksPage = BeautifulSoup(htmlPage.read(), features="html.parser")
 
-            if stocksPage is None:
-                return None
+            if stocksPage is None: return None
 
             stocksTbl = stocksPage.findChildren("table")
+
+            if (stocksTbl is None) or (len(stocksTbl) != 5): continue
+
+            ## START -- REGISTRATION INFO
+            if (stocksTbl[0].contents is None) or (len(stocksTbl[0].contents) != 11): continue
+       
+            ### PRICE
+            if (stocksTbl[0].contents[1].contents is None) or (len(stocksTbl[0].contents[1].contents) != 9): continue
+            stockPrice = stocksTbl[0].contents[1].contents[7].get_text()
+       
+            ### TYPE
+            if (stocksTbl[0].contents[3].contents is None) or (len(stocksTbl[0].contents[3].contents) != 9): continue
+            stockType = stocksTbl[0].contents[3].contents[3].get_text()
+
+            ## END -- REGISTRATION INFO
+
+            ## POPULATING THE LIST
+            stockBasicInfoList["stockType"] = "ON" if str(stockType).upper().find("ON") > 0 else "PN"
+            stockBasicInfoList["stockPrice"] = Parser.ParseFloat(stockPrice)
+
+            returnList.append(stockBasicInfoList)
+
+        return returnList
+
+            
